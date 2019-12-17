51d1d8ad4f02d9fa70f0fbec0e77ce6241e64374
Moe-dulo <58731393+Moe-dulo@users.noreply.github.com>
2019-12-17 15:10:44 +0100

    Float to ASCII

--- Arduino/WiFiUdpSendReceivemax/WiFiUdpSendReceivemax.ino
+++ Arduino/WiFiUdpSendReceivemax/WiFiUdpSendReceivemax.ino
@@ -2,23 +2,24 @@
 /*
   WiFi UDP Send and Receive String
 
- This sketch wait an UDP packet on localPort using the WiFi module.
- When a packet is received an Acknowledge packet is sent to the client on port remotePort
+  This sketch wait an UDP packet on localPort using the WiFi module.
+  When a packet is received an Acknowledge packet is sent to the client on port remotePort
 
- created 30 December 2012
- by dlf (Metodo2 srl)
+  created 30 December 2012
+  by dlf (Metodo2 srl)
 
- */
+*/
 
 
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <WiFiUdp.h>
+#include <Arduino_LSM6DS3.h>
 
 //IP 10.7.21.191
 
 int status = WL_IDLE_STATUS;
-#include "arduino_secrets.h" 
+#include "arduino_secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = "hdpk-student";        // your network SSID (name)
 char pass[] = "drUse300";    // your network password (use for WPA, or use as key for WEP)
@@ -36,6 +37,11 @@ void setup() {
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
   }
 
   // check for the WiFi module:
@@ -70,43 +76,69 @@ void setup() {
 
 void loop() {
 
+  float accelX, accelY, accelZ, pitch, roll;
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(accelX, accelY, accelZ);
+
+    pitch = 180 * atan2(accelX, sqrt(accelY * accelY + accelZ * accelZ)) / PI;
+    roll = 180 * atan2(accelY, sqrt(accelX * accelX + accelZ * accelZ)) / PI;
+
+
+    Serial.print(pitch);
+    Serial.print('\t');
+    Serial.println(roll);
+
+  }
+
   int rand_num = 128; //random(10, 100);
   char cstr[32];
-  itoa(rand_num, cstr, 10);
+  //ftoa(rand_num, cstr, 10);
+  //char src[] = "Pitch ";
+  //char dest[32];
+  //strcat(src, cstr); //src = "Pitch 128";
+
+  char float_string[30];
+  //dtostrf(floatVar, minStringWidthIncDecimalPoint, numVarsAfterDecimal, charBuf);
+  dtostrf(pitch, 2, 2, float_string );
+
+  char keyword[] = "Pitch ";
+  strcat(keyword, float_string);
   
-  char src[] = "Pitch ";
-  char dest[32];
-  strcat(src,cstr); //src = "Pitch 128";
-  
+
+  Serial.println(keyword);
+
+
+
   //strcpy(dest,cstr);
-  
- // send a reply, to the IP address and port that sent us the packet we received
-    Udp.beginPacket("10.7.21.191", 10001);
-    Udp.write(src);
-    Udp.endPacket();
 
-/*
-  // if there's data available, read a packet
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remoteIp = Udp.remoteIP();
-    Serial.print(remoteIp);
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
+  // send a reply, to the IP address and port that sent us the packet we received
+  Udp.beginPacket("10.7.21.191", 10001);
+  Udp.write(keyword);
+  Udp.endPacket();
 
-    // read the packet into packetBufffer
-    int len = Udp.read(packetBuffer, 255);
-    if (len > 0) {
-      packetBuffer[len] = 0;
+  /*
+    // if there's data available, read a packet
+    int packetSize = Udp.parsePacket();
+    if (packetSize) {
+      Serial.print("Received packet of size ");
+      Serial.println(packetSize);
+      Serial.print("From ");
+      IPAddress remoteIp = Udp.remoteIP();
+      Serial.print(remoteIp);
+      Serial.print(", port ");
+      Serial.println(Udp.remotePort());
+
+      // read the packet into packetBufffer
+      int len = Udp.read(packetBuffer, 255);
+      if (len > 0) {
+        packetBuffer[len] = 0;
+      }
+      Serial.println("Contents:");
+      Serial.println(packetBuffer);
+
+
     }
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
-
-   
-  }
   */
 }
 
